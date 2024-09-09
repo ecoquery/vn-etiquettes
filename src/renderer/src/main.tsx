f6777af19387f7bebbde22060a65c03f9f6c2e26
Emmanuel Coquery <emmanuel.coquery@univ-lyon1.fr>
2024-09-09 17:20:30 +0200

    Fix build + affichage imprimante

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,16 +3,22 @@ import './assets/main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import dymoURL from './assets/DYMO.Label.Framework.3.0.txt'
 
 // Load DYMO js lib before building react app
-fetch('/src/assets/DYMO.Label.Framework.3.0.js')
+fetch(dymoURL)
   .then((r) => r.text())
   .then((script) => {
     console.log(`got script, length: ${script.length}`)
     eval(script)
 
+    const printers = dymo.label.framework.getPrinters()
+    const printerInfo = printers.length > 0 ? printers[0].name : "Pas d'imprimante"
+
     ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <React.StrictMode>
+        <p>Salut</p>
+        <p>Imprimante: {printerInfo}</p>
         <App />
       </React.StrictMode>
     )
