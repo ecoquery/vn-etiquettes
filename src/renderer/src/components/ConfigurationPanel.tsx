1135222d8456abbbacebc0aa178b2e3bda7fbd21
Emmanuel Coquery <emmanuel.coquery@univ-lyon1.fr>
2025-10-30 10:16:29 +0100

    Sauvegarde et restauration de la configuration

--- src/renderer/src/components/ConfigurationPanel.tsx
+++ src/renderer/src/components/ConfigurationPanel.tsx
@@ -1,6 +1,9 @@
 import { Button, Container, Stack, TextField } from '@mui/material'
+import { styled } from '@mui/material/styles'
 import { AppDispatch } from '@renderer/app/store'
 import {
+  exportConfiguration,
+  importConfiguration,
   loadConfiguration,
   saveConfiguration,
   selectAnnee,
@@ -8,9 +11,46 @@ import {
 } from '@renderer/features/configuration/configurationSlice'
 import { useDispatch, useSelector } from 'react-redux'
 
+// From https://mui.com/material-ui/react-button/#file-upload
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1
+})
+
+const saveFileOptions = {
+  types: [
+    {
+      suggestedName: 'config.json',
+      description: 'Json Files',
+      accept: {
+        'application/json': ['.json']
+      }
+    }
+  ]
+}
+
 export const ConfigurationPanel = () => {
   const dispatch: AppDispatch = useDispatch()
   const annee = useSelector(selectAnnee)
+  const emptyCfgFile = ''
+  const handleImport = (event) => {
+    if (event.target?.files[0]) {
+      dispatch(importConfiguration(event.target.files[0]))
+    } else {
+      console.error("Pas de fichier pour l'import")
+    }
+  }
+  const handleExport = async () => {
+    const fileHandle = await globalThis.showSaveFilePicker(saveFileOptions)
+    dispatch(exportConfiguration(fileHandle))
+  }
   return (
     <Container>
       <Stack alignItems={'center'} spacing={2}>
@@ -31,14 +71,23 @@ export const ConfigurationPanel = () => {
           >
             Restaurer
           </Button>
+          <Button component="label" variant="contained" role={undefined} tabIndex={-1}>
+            Importer
+            <VisuallyHiddenInput type="file" value={emptyCfgFile} onChange={handleImport} />
+          </Button>
+          <Button variant="contained" onClick={handleExport}>
+            Exporter
+          </Button>
         </Stack>
-        <TextField
-          label="Année"
-          value={annee}
-          onChange={(event) => {
-            dispatch(updateAnnee(event.target.value))
-          }}
-        ></TextField>
+        <Stack alignItems={'normal'}>
+          <TextField
+            label="Année"
+            value={annee}
+            onChange={(event) => {
+              dispatch(updateAnnee(event.target.value))
+            }}
+          />
+        </Stack>
       </Stack>
     </Container>
   )
--- src/renderer/src/features/configuration/configurationSlice.ts
+++ src/renderer/src/features/configuration/configurationSlice.ts
@@ -34,6 +34,25 @@ export const loadConfiguration: AppThunk = (dispatch, _getState) => {
   dispatch(configurationSlice.actions.updateWholeConfiguration(configData))
 }
 
+export const importConfiguration =
+  (file: File): AppThunk =>
+  async (dispatch, _getState) => {
+    const jsonData = await file.text()
+    const configData = JSON.parse(jsonData)
+    dispatch(configurationSlice.actions.updateWholeConfiguration(configData))
+    dispatch(saveConfiguration)
+  }
+
+export const exportConfiguration =
+  (fileHandle: FileSystemFileHandle): AppThunk =>
+  async (_dispatch, getState) => {
+    const state = getState().configuration
+    const stringData = JSON.stringify(state, undefined, 2)
+    const writable = await fileHandle.createWritable()
+    await writable.write(stringData)
+    await writable.close()
+  }
+
 export const { updateAnnee } = configurationSlice.actions
 export default configurationSlice.reducer
 export const selectAnnee = (state: RootState) => state.configuration.annee
