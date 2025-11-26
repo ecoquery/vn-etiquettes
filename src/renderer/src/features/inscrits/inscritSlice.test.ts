ca1fce9c9b3615bd1630d37bac9ac21d54f25637
Emmanuel Coquery <emmanuel.coquery@univ-lyon1.fr>
2025-11-26 08:40:06 +0100

    Fix tests in incritSlice

--- src/renderer/src/features/inscrits/inscritSlice.test.ts
+++ src/renderer/src/features/inscrits/inscritSlice.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { extractCreneaux, extractPiscine, extractTitreCourt } from './inscritsSlice'
+import { readFile } from 'node:fs/promises'
 
 const sampleCreneaux = [
   'Centre Nautique Etienne Gagnaire Mercredi : 20h00 - 20h55',
@@ -25,11 +26,18 @@ const sampleGroupes = [
   'ADULTES DEBUTANTS-Gratte ciel'
 ]
 
+async function loadConfigFromFile() {
+  const configData = await readFile('config-etiquettes.json', { encoding: 'utf8' })
+  return JSON.parse(configData)
+}
+
+const config = await loadConfigFromFile()
+
 describe('extractCreneau', () => {
   it('should start with a day', () => {
     const shortDays = days.map((s) => s.substring(0, 3))
     for (const cr of sampleCreneaux) {
-      const creneau = extractCreneaux(cr)[0]
+      const creneau = extractCreneaux(cr, config)[0]
       if (creneau !== undefined) {
         expect(shortDays.some((day) => creneau.heure.startsWith(day))).toBeTruthy()
       }
@@ -37,7 +45,7 @@ describe('extractCreneau', () => {
   })
   it('should contain a time', () => {
     for (const cr of sampleCreneaux) {
-      const creneau = extractCreneaux(cr)[0]
+      const creneau = extractCreneaux(cr, config)[0]
       if (creneau !== undefined) {
         expect(creneau.heure).toSatisfy((c) => Boolean(/.*\d+h\d\d.*/.exec(c)))
       }
@@ -49,7 +57,7 @@ describe('extractPiscine', () => {
   it('should give a piscine name', () => {
     const piscineOk = ['CNEG', 'Boulloche']
     for (const cr of sampleCreneaux) {
-      const piscine = extractPiscine(cr)
+      const piscine = extractPiscine(cr, config)
       expect(piscine).toSatisfy((p) => piscineOk.indexOf(p) >= 0 || p === undefined)
     }
   })
@@ -58,13 +66,13 @@ describe('extractPiscine', () => {
 describe('extractTitreCourt', () => {
   it('should parse a variety of groupes', () => {
     for (const gr of sampleGroupes) {
-      expect(extractTitreCourt(gr)).not.toEqual('erreur')
+      expect(extractTitreCourt(gr, config)).not.toEqual('erreur')
     }
   })
   it('should extract the data from group', () => {
-    expect(extractTitreCourt('ADU10 -3-4 nages')).toStrictEqual('ADU10')
+    expect(extractTitreCourt('ADU10 -3-4 nages', config)).toStrictEqual('ADU10')
   })
   it('should replace the data when needed', () => {
-    expect(extractTitreCourt('ADULTES DEBUTANTS-Boulloche')).toStrictEqual('ADUDEB')
+    expect(extractTitreCourt('ADULTES DEBUTANTS-Boulloche', config)).toStrictEqual('ADUDEB')
   })
 })
