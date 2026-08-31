e73195d1c5a66039d0428d841d477c745e3a0fc9
Emmanuel Coquery <emmanuel.coquery@univ-lyon1.fr>
2026-08-31 13:26:18 +0200

    Import excel + csv

--- src/renderer/src/components/CreneauxPanel.tsx
+++ src/renderer/src/components/CreneauxPanel.tsx
@@ -1,42 +1,13 @@
-import { Button, Container, Stack, Typography } from '@mui/material'
+import { Container, Stack, Typography } from '@mui/material'
 import type { GridColDef } from '@mui/x-data-grid'
 import { DataGrid } from '@mui/x-data-grid'
-import type { CSSProperties } from 'react'
-import { useCSVReader } from 'react-papaparse'
 import { useDispatch, useSelector } from 'react-redux'
 import type { AppDispatch } from '../app/store'
 import type { Activite } from '../features/creneaux/creneauxSlice'
 import { importCreneauxWithData, selectCreneaux } from '../features/creneaux/creneauxSlice'
-
-const styles = {
-  csvReader: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginBottom: 10,
-    padding: 10
-  } as CSSProperties,
-  browseFile: {
-    width: '200px',
-    margin: 10
-  } as CSSProperties,
-  acceptedFile: {
-    // border: '1px solid #ccc',
-    height: 45,
-    lineHeight: 2.5,
-    paddingLeft: 10,
-    width: '500px'
-  } as CSSProperties,
-  remove: {
-    borderRadius: 0,
-    padding: '0 20px'
-  } as CSSProperties,
-  progressBarBackgroundColor: {
-    backgroundColor: 'blue'
-  } as CSSProperties
-}
+import SheetReader from './SheetReader'
 
 export const CreneauxPanel = () => {
-  const { CSVReader } = useCSVReader()
   const dispatch: AppDispatch = useDispatch()
   const creneaux = useSelector(selectCreneaux)
   // Données des créneaux sous forme de tableau
@@ -63,24 +34,11 @@ export const CreneauxPanel = () => {
             // onPaginationModelChange={setPaginationModel}
           />
         </div>
-        <CSVReader
-          onUploadAccepted={(results: any) => {
-            dispatch(importCreneauxWithData(results.data))
+        <SheetReader
+          onDataLoaded={(data) => {
+            dispatch(importCreneauxWithData(data))
           }}
-          config={{ header: true }}
-        >
-          {({ getRootProps, acceptedFile, ProgressBar }: any) => (
-            <>
-              <div style={styles.csvReader}>
-                <Button variant="contained" {...getRootProps()} style={styles.browseFile}>
-                  Importer les créneaux
-                </Button>
-                <div style={styles.acceptedFile}>{acceptedFile?.name}</div>
-              </div>
-              <ProgressBar style={styles.progressBarBackgroundColor} />
-            </>
-          )}
-        </CSVReader>
+        />
       </Stack>
     </Container>
   )
---
+++ src/renderer/src/components/SheetReader.tsx
@@ -0,0 +1,65 @@
+import { Box, Button, Typography } from '@mui/material'
+import { ChangeEvent, useState } from 'react'
+import Papa from 'papaparse'
+import * as XLSX from 'xlsx'
+
+interface SheetData {
+  [key: string]: string // | number | boolean | null;
+}
+
+const SheetReader = ({ onDataLoaded }: { onDataLoaded: (data: SheetData[]) => void }) => {
+  const [data, setData] = useState<SheetData[]>([])
+  const [fileName, setFileName] = useState<string>('')
+  const changeData = (data) => {
+    setData(data)
+    onDataLoaded(data)
+  }
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (!file) return
+
+    setFileName(file.name)
+
+    if (file.name.endsWith('.csv')) {
+      Papa.parse(file, {
+        header: true,
+        delimiter: ';',
+        complete: (results) => {
+          changeData(results.data)
+        },
+        error: (error) => {
+          console.error('Erreur lors du parsing :', error)
+        }
+      })
+    } else {
+      try {
+        const arrayBuffer = await file.arrayBuffer()
+
+        // Lire le contenu du fichier Excel
+        const workbook = XLSX.read(arrayBuffer, { type: 'array' })
+        const worksheetName = workbook.SheetNames[0] // Prendre la première feuille
+        const worksheet = workbook.Sheets[worksheetName]
+        const jsonData: SheetData[] = XLSX.utils.sheet_to_json(worksheet)
+        changeData(data)
+      } catch (error) {
+        console.error('Erreur lors de la lecture du fichier :', error)
+      }
+    }
+  }
+
+  return (
+    <Box sx={{ padding: 3 }}>
+      <Button variant="contained" component="label" sx={{ marginBottom: 2 }}>
+        Importer (xlsx ou csv)
+        <input type="file" hidden accept=".xlsx, .xls, .csv" onChange={handleFileChange} />
+      </Button>
+
+      {fileName && (
+        <Typography variant="body1" gutterBottom>
+          Fichier sélectionné : {fileName}
+        </Typography>
+      )}
+    </Box>
+  )
+}
+export default SheetReader
